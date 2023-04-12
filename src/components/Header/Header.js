31cb18037edfc5776e29bcdce76c1a9e399338b4
Daniel Andersson <da222xg@student.lnu.se>
2023-04-12 20:55:54 +0200

    Create hero section.

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,15 +1,16 @@
 import Link from 'next/link';
 import React from 'react';
 import { AiFillCloseSquare, AiFillGithub, AiFillInstagram, AiFillLinkedin } from 'react-icons/ai';
-import { DiCssdeck } from 'react-icons/di';
+import { BsCode, BsCodeSlash } from 'react-icons/bs';
+import { FaLaptopCode } from 'react-icons/fa'
 
 import { Container, Span, Div1, Div2, Div3, NavLink, SocialIcons } from './HeaderStyles';
 
 const Header = () =>  (
   <Container>
     <Div1>
-      <Link href='/' style={{ display: 'flex', alignItems: 'center', color: 'whitesmoke', marginBottom: '20px' }}>
-          <DiCssdeck size="3rem" /> <Span>Portfolio</Span>
+      <Link href='/' style={{ display: 'flex', alignItems: 'center', color: 'rgba(255, 255, 255, 0.75)', marginBottom: '20px' }}>
+          <FaLaptopCode size="3rem" style={{ marginRight: '4px' }} /> <Span>Portfolio</Span> 
       </Link>
     </Div1>
     <Div2>
@@ -26,6 +27,11 @@ const Header = () =>  (
       <li>
         <Link href='#projects'>
           <NavLink>About</NavLink>
+        </Link>
+      </li>
+      <li>
+        <Link href='#projects'>
+          <NavLink>Contact</NavLink>
         </Link>
       </li>
     </Div2>
--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -5,9 +5,19 @@ import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
 const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle main center>
+        quote? slogan
+      </SectionTitle>
+      <SectionText>
+        Hi, i'm Daniel. <br/>
+        Currently a student at Linneus University pursuing a B.Sc degree in web development.
+        I'm 
+      </SectionText>
+      <Button onClick={() => window.location = '' }></Button>
+    </LeftSection>
+  </Section>
 );
 
 export default Hero;
